8f8fec6b24441887c5821dc9270d5e43d2ba4150
Юля <julia.leks2005@gmail.com>
2025-04-01 15:24:14 +0500

    Добавила класс WindowSaver, который используется для сохранения и управления параметрами окон и интерфейс WindowAction

---
+++ src/main/java/state/WindowAction.java
@@ -0,0 +1,26 @@
+package state;
+import java.util.Map;
+
+/**
+ * Сохраняет и восстанавливает состояние окна
+ */
+public interface WindowAction {
+
+    /**
+     * Сохраняет состояние окна
+     * @return Возвращает мапу, в которой в ключе лежит название
+     * параметра, а в значение - состояние
+     */
+    Map<String, Integer> saveWindowState();
+
+    /**
+     * Загружает состояние окна
+     */
+    void loadWindowState(Map<String, Integer> params);
+
+    /**
+     *
+     * @return Возвращает имя окна
+     */
+    String getNameOfWindow();
+}
